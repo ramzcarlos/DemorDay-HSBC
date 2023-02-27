aa95b580a5560c51a3c86b70b9e7cfe2679a8f8d
Carlos Ramirez <carlos.ramz.pina@gmail.com>
2023-02-27 14:07:12 -0600

    test

--- backend-sismos/src/test/java/Hsbcbeducrp/backendsismos/models/CalculosTest.java
+++ backend-sismos/src/test/java/Hsbcbeducrp/backendsismos/models/CalculosTest.java
@@ -10,28 +10,28 @@ class CalculosTest {
     @Test
     @DisplayName("Prueba del metodo riguidez")
     void reguidezTest(){
-        assertEquals(2.4672, calculos.riguidez(4));
+        assertEquals(2.4674010276794434, calculos.riguidez(4));
     }
 
     @Test
     @DisplayName("prueba del metodo amortiguamiento")
     void amortiguamientoTest(){
-        assertEquals(5.12, calculos.amortiguamiento(0.1F,  2.4472F));
+        assertEquals(0.312870591878891, calculos.amortiguamiento(0.1F,  2.4472F));
     }
 
     @Test
     @DisplayName("prueba del metodo fun a1")
     void fun_a1Test(){
-        assertEquals(2.4562, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
+        assertEquals(0.21736648678779602, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
     }
     @Test
     @DisplayName("prueba del metodo fun a2")
     void fun_a2Test(){
-        assertEquals(0.4562, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
+        assertEquals(0.21736648678779602, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
     }
     @Test
     @DisplayName("prueba del metodo fun a2")
     void fun_a3Test(){
-        assertEquals(1.2562, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
+        assertEquals(0.21736648678779602, calculos.fun_a1(2.36F, 3.12F, 0.25F, 5.12F));
     }
 }
